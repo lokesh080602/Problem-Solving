d7163eb0a0c503a94a7a520f3e9a45b8b4820ed9
lokesh.v <lokesh.v@User4.teleapps.com>
2024-08-20 18:15:54 +0530

    Code Written

--- JavaProgram/src/sortingAlgorithm/BubbleSort.java
+++ JavaProgram/src/sortingAlgorithm/BubbleSort.java
@@ -1,10 +1,22 @@
 package sortingAlgorithm;
 
+import java.util.Arrays;
+
 public class BubbleSort {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int arr[] = {64,25,12,22,11};
+		for(int i=0; i<arr.length; i++) {
+			for(int j=0; j<arr.length-i-1; j++) {
+				if(arr[j]>arr[j+1]) {
+					int temp = arr[j+1];
+					arr[j+1] = arr[j];
+					arr[j] = temp;
+				}
+			}
+		}
+		System.out.println(Arrays.toString(arr));
 	}
 
 }
